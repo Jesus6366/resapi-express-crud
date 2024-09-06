5fead966d1f63c919bd73cda96d6aa4315c0cda0
unknown <polibos6366@gmail.com>
2024-09-05 20:04:50 -0600

    put and delete endpoint

--- server.js
+++ server.js
@@ -2,30 +2,72 @@ const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
+let products = [
+  {
+    id: 1,
+    name: "laptop",
+    price: 3000,
+  },
+];
 
 // midlewares
 app.use(morgan("dev"));
+app.use(express.json());
 
 // endpoints / routes
 
 app.get("/products", (req, res) => {
-  res.send("Getting products");
+  res.json(products);
 });
 
 app.post("/products", (req, res) => {
-  res.send("Creating products");
+  const newProducts = { ...req.body, id: products.length + 1 };
+  products.push(newProducts);
+
+  res.send(newProducts);
 });
 
-app.put("/products", (req, res) => {
-  res.send("Updating products");
+app.put("/products/:id", (req, res) => {
+  const newData = req.body;
+  const productFound = products.find((product) => {
+    return product.id === Number(req.params.id);
+  });
+
+  if (!productFound) {
+    return res.status(404).json({ message: "Product not found" });
+  }
+
+  products = products.map((p) =>
+    p.id === Number(req.params.id) ? { ...p, ...newData } : p
+  );
+
+  res.json(products);
 });
 
-app.delete("/products", (req, res) => {
-  res.send("Deleting products");
+app.delete("/products/:id", (req, res) => {
+  const productFound = products.find((product) => {
+    return product.id === Number(req.params.id);
+  });
+
+  if (!productFound) {
+    return res.status(404).json({ message: "Product not found" });
+  }
+
+  products = products.filter((p) => p.id !== Number(req.params.id));
+
+  res.sendStatus(204);
 });
 
 app.get("/products/:id", (req, res) => {
-  res.send("Getting one product");
+  const productFound = products.find((product) => {
+    return product.id === Number(req.params.id);
+  });
+
+  if (!productFound) {
+    return res.status(404).json({ message: "Product not found" });
+  }
+
+  res.json(productFound);
 });
 
 const port = 3000;
